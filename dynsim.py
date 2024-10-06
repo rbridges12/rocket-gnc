fc0e247d7fc41f86013c1f88dd8dac359ff3b79f
Riley Bridges <rlybrdgs@gmail.com>
2024-10-06 17:20:12 -0400

    working animation

--- dynsim.py
+++ dynsim.py
@@ -2,14 +2,44 @@ import numpy as np
 from pyatmos import nrlmsise00, coesa76
 from scipy.interpolate import pchip_interpolate 
 
+# constants
 g = 9.81
 R = 287.1
 gamma = 1.4
+data0 = coesa76(0)
+rho0 = data0.rho
+# T0 = data0.T
+# P0 = data0.P
+# a0 = np.sqrt(gamma * R * T0)
 
-SP = 2.3
+# pursuer and evader surface areas (m^2)
+SP = 2.3 
 SE = 28
+
+# pursuer and evader masses (kg)
 mP = 130
 mE = 10000
+
+# pursuer and evader drag coefficient vs Mach number data
+MiP = np.array([0, 0.6, 0.8, 1, 1.2, 2, 3, 4, 5])
+CdiP = np.array([0.016, 0.016, 0.0195, 0.045, 0.039, 0.0285, 0.024, 0.0215, 0.020])
+MiE = np.array([0, 0.9, 1, 1.2, 1.6, 2])
+CdiE = np.array([0.0175, 0.019, 0.05, 0.045, 0.043, 0.038])
+
+evader_thrust0 = 76310
+
+# turbojet thrust profile
+def turbojet_thrust(T0, rho):
+    return (rho/rho0) * T0
+
+# boost-sustain solid rocket motor thrust profile
+def SRM_thrust(t):
+    if t < 10:
+        return 10000
+    elif t < 30:
+        return 1800
+    else:
+        return 0
 
 
 def dynsim(x, t, nzE, nzP):
@@ -27,34 +57,18 @@ def dynsim(x, t, nzE, nzP):
     TE = data_E.T
     PE = data_E.P
     aE = np.sqrt(gamma * R * TE)
-    data0 = coesa76(0)
-    rho0 = data0.rho
-    T0 = data0.T
-    P0 = data0.P
-    a0 = np.sqrt(gamma * R * T0)
     
     # calculate drag coefficients by interpolating measured data
-    MiP = np.array([0, 0.6, 0.8, 1, 1.2, 2, 3, 4, 5])
-    CdiP = np.array([0.016, 0.016, 0.0195, 0.045, 0.039, 0.0285, 0.024, 0.0215, 0.020])
     CdP = pchip_interpolate(MiP, CdiP, VP/aP)
-    MiE = np.array([0, 0.9, 1, 1.2, 1.6, 2])
-    CdiE = np.array([0.0175, 0.019, 0.05, 0.045, 0.043, 0.038])
     CdE = pchip_interpolate(MiE, CdiE, VE/aE)
     
-    # turbofan thrust approximation (N)
-    T0 = 76310
-    TE = (rhoE/rho0) * T0
+    TE = turbojet_thrust(evader_thrust0, rhoE)
+    TP = SRM_thrust(t)
 
-    # solid rocket motor thrust profile
-    TP = 0
-    if t < 10:
-        TP = 10000
-    elif t < 30:
-        TP = 1800
     
     # saturate normal accelerations at 40g and 10g
     nzP = np.clip(nzP, -40*g, 40*g)
-    # nzE = np.clip(nzE, -10*g, 10*g)
+    nzE = np.clip(nzE, -10*g, 10*g)
 
     dx[0] = (TP - 0.5 * rhoP * VP**2 * CdP * SP) / mP - g * np.sin(gammaP)
     dx[1] = -(1/VP) * (nzP + g * np.cos(gammaP))
--- intercept.py
+++ intercept.py
@@ -1,11 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 from scipy.integrate import solve_ivp
 
 from dynsim import dynsim
 
 # plt.rcParams["text.usetex"] = True
 
+g = 9.81
 # x = [VP, gammaP, hP, dP, VE, gammaE, hE, dE]
 
 def output(x):
@@ -14,43 +16,9 @@ def output(x):
     betadot = ((x[7] - x[3])*(x[4]*np.sin(x[5]) - x[0]*np.sin(x[1])) - (x[6] - x[2])*(x[4]*np.cos(x[5]) - x[0]*np.cos(x[1])))/( (x[6] - x[2])**2 + (x[7] - x[3])**2)
     return np.array([R, Rdot, betadot])
 
-# function [detonate , missDistance ] = fuze( xx )
-# % returns detonate = 1 and miss distance if fuzing conditions have been met.
-# % returns detonate = 0 and miss distance = Nan if fuzing conditions have
-# % NOT been met
-# % Written by Syed Aseem Ul Islam (aseemisl@umich.edu)
-# %
-# R = sqrt( ( xx(:,7)-xx(:,3) ).^2 + ( xx(:,8)-xx(:,4) ).^2  );
-# temp1a = ( abs(R(2:end,:)) < 10); %Is R<10 in intersample?
-# temp1b = ( R(2:end,:).*R(1:end-1,:) < 0) ; % Is R crossing 0 in intersample?
-# temp1c = ( R(2:end,:)-R(1:end-1,:) > 0) ; % Is R increasing in intersample?
-# temp1bc = any( [temp1b temp1c] , 2 );
-# temp1 = all( [temp1a temp1bc] , 2 ); %Is R<10 AND R crossing 0 or Rdot>0 in intersample?
-# temp2 = xx(2:end,3) < 0; %Did pursuer hit the ground in intersample?
-# temp3 = xx(2:end,7) < 0; %Did evader hit the ground in intersample?  
-# temp4 = xx(2:end,1) < 0; %Did pursuer slow to 0 speed in intersample?
-# temp5 = xx(2:end,5) < 0; %Did evader slow to 0 speed in intersample?
-# temp = any( [temp1 temp2 temp3 temp4 temp5] , 2); %Is any fuzing condition satisfied in intersample?
-# if any(temp,1)
-#     fuze_index = 0;
-#     for kk = 1:length(temp)
-#         if temp(kk) == 1
-#             fuze_index = kk;
-#             break
-#         end
-#     end
-#     missDistance = R(fuze_index,:);
-#     detonate = 1;
-# else
-#     missDistance = NaN;
-#     detonate = 0;
-# end
-
-# end
-
 def fuze(xsim):
     R = np.sqrt((xsim[6, :] - xsim[2, :])**2 + (xsim[7, :] - xsim[3, :])**2)
-    print(np.min(R))
+    # print(np.min(R))
     temp1a = np.abs(R[1:]) < 10
     temp1b = R[1:] * R[:-1] < 0
     temp1c = R[1:] - R[:-1] > 0
@@ -58,7 +26,7 @@ def fuze(xsim):
     # print(np.stack([temp1b, temp1c], axis=1).shape)
     temp1bc = np.any(np.stack([temp1b, temp1c], axis=1), axis=1)
     temp1 = np.all(np.stack([temp1a, temp1bc], axis=1), axis=1)
-    print(np.any(temp1a), np.any(temp1bc), np.any(temp1))
+    # print(np.any(temp1a), np.any(temp1bc), np.any(temp1))
     temp2 = xsim[2, 1:] < 0
     temp3 = xsim[6, 1:] < 0
     temp4 = xsim[0, 1:] < 0
@@ -78,26 +46,91 @@ def fuze(xsim):
         detonate = 0
     return detonate, missDistance
 
-def plot_trajectories(x, i):
-    plt.plot(x[3, :i]/1000, x[2, :i]/1000, 'b')
+def plot_intercept(x):
+    fig = plt.figure()
+    plt.plot(x[3, :]/1000, x[2, :]/1000, 'b')
     plt.plot(x[3, 0]/1000, x[2, 0]/1000, 'bx')
-    plt.plot(x[3, i]/1000, x[2, i]/1000, 'bo')
-    plt.plot(x[7, :i]/1000, x[6, :i]/1000, 'r')
+    plt.plot(x[3, -1]/1000, x[2, -1]/1000, 'bo')
+    plt.plot(x[7, :]/1000, x[6, :]/1000, 'r')
     plt.plot(x[7, 0]/1000, x[6, 0]/1000, 'rx')
-    plt.plot(x[7, i]/1000, x[6, i]/1000, 'ro')
+    plt.plot(x[7, -1]/1000, x[6, -1]/1000, 'ro')
     plt.grid()
     ylimits = plt.ylim()
     plt.ylim([0, ylimits[1]])
     plt.xlabel(r"$d$ (km)")
     plt.ylabel(r"$h$ (km)")
     plt.legend(["Pursuer", "Evader"])
-    plt.show()
+
+def animate_intercept(xs):
+    VPs = xs[0, :]
+    gammaPs = xs[1, :]
+    hPs = xs[2, :] / 1000
+    dPs = xs[3, :] / 1000
+    VEs = xs[4, :]
+    gammaEs = xs[5, :]
+    hEs = xs[6, :] / 1000
+    dEs = xs[7, :] / 1000
+
+    x_margin = 1
+    y_margin = 1
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, np.max([np.max(dPs), np.max(dEs)]) + x_margin)
+    ax.set_ylim(0, np.max([np.max(hPs), np.max(hEs)]) + y_margin)
+    ax.set_aspect('equal')
+    ax.grid()
+    pursuer, = ax.plot([], [], 'bo')
+    evader, = ax.plot([], [], 'ro')
+    pursuer_path, = ax.plot([], [], 'b-')
+    evader_path, = ax.plot([], [], 'r-')
+    
+    def animate(i):
+        pursuer.set_data([dPs[i]], [hPs[i]])
+        evader.set_data([dEs[i]], [hEs[i]])
+        pursuer_path.set_data(dPs[:i], hPs[:i])
+        evader_path.set_data(dEs[:i], hEs[:i])
+        return pursuer, evader, pursuer_path, evader_path
+    
+    ani = animation.FuncAnimation(fig, animate, frames=xs.shape[1], interval=50, repeat=True)
+    return ani
+        
+
+
+def plot_trajectories(xs, ts):
+    VPs = xs[0, :]
+    gammaPs = xs[1, :]
+    hPs = xs[2, :]
+    dPs = xs[3, :]
+    VEs = xs[4, :]
+    gammaEs = xs[5, :]
+    hEs = xs[6, :]
+    dEs = xs[7, :]
+    
+    fig, axs = plt.subplots(2, 2, sharex=True)
+    axs[0, 0].plot(ts, dPs, "r-", label="Pursuer")
+    axs[0, 0].plot(ts, dEs, "b-", label="Evader")
+    axs[0, 0].set_ylabel("Downrange (m)")
+    axs[0, 0].set_xlabel("Time (s)")
+    axs[0, 0].legend()
+    
+    axs[0, 1].plot(ts, hPs, "r-", label="Pursuer")
+    axs[0, 1].plot(ts, hEs, "b-", label="Evader")
+    axs[0, 1].set_ylabel("Altitude (m)")
+    axs[0, 1].set_xlabel("Time (s)")
+    
+    axs[1, 0].plot(ts, VPs, "r-", label="Pursuer")
+    axs[1, 0].plot(ts, VEs, "b-", label="Evader")
+    axs[1, 0].set_ylabel("Speed (m/s)")
+    axs[1, 0].set_xlabel("Time (s)")
+    
+    axs[1, 1].plot(ts, gammaPs, "r-", label="Pursuer")
+    axs[1, 1].plot(ts, gammaEs, "b-", label="Evader")
+    axs[1, 1].set_ylabel("Flight Path Angle (rad)")
+    axs[1, 1].set_xlabel("Time (s)")
+    
 
 def simulate_engagement(x0):
     duration = 50
     dt = 0.1
-    g = 9.81
-    umax = 40 * g
     nzE = -g
     
     # loft conditions
@@ -106,6 +139,7 @@ def simulate_engagement(x0):
     loft = delta_h0 > -100 and delta_d0 > 2000
     
     xs = np.zeros((8, int(duration/dt) + 1))
+    ts = np.arange(0, duration + dt, dt)
     nzPs = np.zeros(int(duration/dt) + 1)
     xs[:, 0] = x0
     
@@ -129,19 +163,18 @@ def simulate_engagement(x0):
                 nzPs[i+1] = nzP_PG(10)
         else:
             nzPs[i+1] = nzP_PG(5)
-        # nzPs[i+1] = nzP_PG(5)
-        # print(nzPs[i+1])
-        # nzPs[i+1] = -1.1 * g
-        # print(sol.t.shape)
         detonate, missDistance = fuze(xsim)
-        print(f"t = {t} s, miss distance = {missDistance} m")
+        # print(f"t = {t} s, miss distance = {missDistance} m")
         if detonate:
+            ts = ts[:i+1]
+            xs = xs[:, :i+1]
+            nzPs = nzPs[:i+1]
             print(f"Detonated at t = {t} s, miss distance = {missDistance} m")
             if loft:
                 print("Loft initiated\n")
             break
         
-    return xs, nzPs, i
+    return xs, ts, nzPs
 
         
 x0_dyn1 = np.array([450, 0, 10000, 0, 450, 0, 10000, 6500])
@@ -158,8 +191,10 @@ x0_dyns = [x0_dyn1, x0_dyn2, x0_dyn3, x0_dyn4, x0_dyn5, x0_dyn6, x0_dyn7, x0_dyn
 
     
 for i in range(10):
-    x, nzPs, ii = simulate_engagement(x0_dyns[i])
-    plot_trajectories(x, ii)
+    x, ts, nzPs = simulate_engagement(x0_dyns[i])
+    # plot_trajectories(x, ts)
+    # plot_intercept(x)
+    ani = animate_intercept(x)
+    plt.show()
     # R = np.sqrt((x[3, ii] - x[7, ii])**2 + (x[2, ii] - x[6, ii])**2)
     # plt.title(f'Initial Condition {i}, Miss Distance = {R} m')
-    plt.show()
